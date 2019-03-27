58de489415e6505601f567ad53ccf68e354026e7
xyxyxchild <976044934@qq.com>
2019-03-27 19:28:45 +0800

    alter js2

--- js/task2/js/Task2-role-num.js
+++ js/task2/js/Task2-role-num.js
@@ -1,58 +1,28 @@
 //总人数输入脚本
-function sliderNum(playerNum) {//滑块移动发生的脚本
-    document.getElementById("writeNum").value= playerNum ;//滑块总人数与键盘输入总人数数值同步
-    minPlayerNum(playerNum);//判断最少人数
-
-
+function sliderNum(sliderValue) {//滑块移动发生的脚本
+    document.getElementById("writeNum").value= sliderValue ;//滑块总人数与键盘输入总人数数值同步
+    minPlayerNum(sliderValue);//判断杀手和平民人数
 }
-function writeNum(playerNum) {//书写发生的脚本
-    document.getElementById("sliderNum").value= playerNum ;//键盘输入总人数与滑块总人数数值同步
-    minPlayerNum(playerNum);//判断最少人数
-
+function writeNum(writenValue) {//书写发生的脚本
+    document.getElementById("sliderNum").value= writenValue ;//键盘输入总人数与滑块总人数数值同步
+    minPlayerNum(writenValue);//判断杀手和平民人数
 }
-//判断最少人数
-  function minPlayerNum(playerNum) {
-    if(playerNum>=4) {
-        killerNum=1;
-        if(playerNum>5){
-            killerNum=2;
-            if(playerNum>10){
-                killerNum=3;
-                if(playerNum>15){
-                    killerNum=4;
-                }
-            }
-        }
-    }
-
-    civilianNum =playerNum-killerNum;
-    document.getElementById("killerNum").innerHTML= "杀&nbsp;&nbsp;&nbsp;&nbsp;手&nbsp;&nbsp;" + killerNum + "&nbsp;&nbsp;人;";
-    document.getElementById("civilianNum").innerHTML= "平&nbsp;&nbsp;&nbsp;&nbsp;民&nbsp;&nbsp;" + civilianNum + "&nbsp;&nbsp;人;";
-      console.log(killerNum);
+//判断杀手和平民人数 var killerNum,civilianNum;
+function minPlayerNum(playerNum) {
+    killerNum = Math.ceil(playerNum/5);
+    civilianNum = playerNum-killerNum;
+    document.getElementById("killerNum").innerHTML=killerNum;
+    document.getElementById("civilianNum").innerHTML= civilianNum ;
     return killerNum;
-
-
 }
 //滑块input点击改变数值
 function reduceNum() {
-    a = document.getElementById("sliderNum").value - 1;//点击则-1
-    if (a<4){//消除点击后会出现的3的bug
-        a=4;
-        return a;
-    }
-    document.getElementById("sliderNum").value = a;//将数值赋予value
-    sliderNum(a);
-    // document.getElementById("sliderNum").value = document.getElementById("sliderNum").value - 1;//形式1
-    // sliderNum(document.getElementById("sliderNum").value);
+    document.getElementById("sliderNum").value = document.getElementById("sliderNum").value - 1;//形式1
+    sliderNum(document.getElementById("sliderNum").value);
 }
 function addNum() {
-    // var b = parseFloat(document.getElementById("sliderNum").value);//形式2
-    // b = b + 1;
-    // document.getElementById("sliderNum").value = b;
-    // sliderNum(b);
     document.getElementById("sliderNum").value =parseFloat( document.getElementById("sliderNum").value) + 1;//parseFloat数值化，而不让+进行字符串相连
     sliderNum(document.getElementById("sliderNum").value);
-
 }
 //输入总人数input数值监听
 function monitorNum(playerNum) {
@@ -67,41 +37,69 @@ function monitorNum(playerNum) {
         sliderNum(playerNum);
     }
 }
-
-
-function getNumArray(arr) {
-    var arr=document.getElementById("writeNum").value;
-    var numArray = new Array(parseFloat(arr)).fill("平民");
-    var killerArray = numArray.slice(0),count=parseFloat(killerNum);
-    for (i=0;i < count;i++) {
+//打乱数组
+function getNumArray() {
+    var arr = document.getElementById("writeNum").value;//获取总人数值
+    var numArray = new Array(parseFloat(arr)).fill("平民");//创建长度为总人数值，全部为平民的数组
+    var killerArray = numArray.slice(0),count=parseFloat(killerNum);//开始得到有杀手的数组，count为杀人数
+    for (i=0;i < count;i++) {//将杀手换进数组，得到杀人数组
         killerArray[i] = "杀手";
     }
-    var shuffle = killerArray.slice(0), l = killerArray.length, temp,index;
-    for (l>=0;l--;) {
+    var shuffle = killerArray.slice(0), l = killerArray.length, temp,index;//开始打乱数组
+    for (l>=0;l--;) {//打乱l次后输出结果
         index = Math.floor((l+1) * Math.random());
         temp = shuffle[index];
         shuffle[index] = shuffle[l];
         shuffle[l] = temp;
     }
-    console.log(shuffle);
     return shuffle;
 }
-//开始发牌脚本
+//开始发牌
 function begin() {
-    getNumArray();
+    getNumArray(); //开始打乱数组
     window.location.href = "Task2-check-role.html";
 }
-//返回脚本
+//返回版本选择
 function backLink() {
     window.location.href = "Task2-page.html";
 }
-
-//初始默认人数脚本
-window.onload= onload();
+//初始默认人数
+window.onload = onload();
 function onload() {
     document.getElementById("sliderNum").value=8;
     sliderNum(8);
 }
+//网页字体跟随页面自动变化
+function setRem(){
+    var width = document.body.offsetWidth; //获取当前页面的宽度
+    var nowFont=width/320*16; //设置页面的字体大小
+    var htmlFont=document.getElementsByTagName('html')[0]; //通过标签名称来获取元素
+    htmlFont.style.fontSize =nowFont+"px"; // 给获取到的元素的字体大小赋值
+}
+setRem(); //运行脚本setRem
+window.onresize=setRem;  //监听屏幕变化
+
+//滑块input点击改变数值
+// function reduceNum() {
+//     // a = document.getElementById("sliderNum").value - 1;//点击则-1
+//     // if (a<4){//消除点击后会出现的3的bug
+//     //     a=4;
+//     //     return a;
+//     // }
+//     // document.getElementById("sliderNum").value = a;//将数值赋予value
+//     // sliderNum(a);
+//     document.getElementById("sliderNum").value = document.getElementById("sliderNum").value - 1;//形式1
+//     sliderNum(document.getElementById("sliderNum").value);
+// }
+// function addNum() {
+//     //  b = parseFloat(document.getElementById("sliderNum").value) + 1;//形式2
+//     // document.getElementById("sliderNum").value = b;
+//     // sliderNum(b);
+//     document.getElementById("sliderNum").value =parseFloat( document.getElementById("sliderNum").value) + 1;//parseFloat数值化，而不让+进行字符串相连
+//     sliderNum(document.getElementById("sliderNum").value);
+//
+// }
+
 
 
 
